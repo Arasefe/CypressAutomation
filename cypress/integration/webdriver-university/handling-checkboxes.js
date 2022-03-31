3d58e8c9156c82d6a5fbd550479320e10a259c90
Arasefe <ismail_yildirim69@yahoo.com>
2022-03-30 19:35:03 -0500

    Hooks added

--- cypress/integration/webdriver-university/handling-checkboxes.js
+++ cypress/integration/webdriver-university/handling-checkboxes.js
@@ -1,41 +1,28 @@
 /// <reference types="cypress" />
 
-describe("Handling checkboxes", () => {
-  it("Check and validate checkboxes", () => {
-    // Open webdriveruniversity homepage
+describe("Verify checkboxes via webdriveruni", () => {
+  beforeEach(function () {
     cy.visit("http://www.webdriveruniversity.com");
-    // In order for openning the tab inside the current window we need to remove the target attribute and value JQuery Method of removeAttrb
     cy.get("#dropdown-checkboxes-radiobuttons")
       .invoke("removeAttr", "target")
       .click({ force: true });
-    // cy.get("#checkboxes > :nth-child(1) > input").check().should("be.checked");
+  });
+  it("Check and validate checkbox", () => {
+    //cy.get('#checkboxes > :nth-child(1) > input').check()
+    //cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked')
 
-    cy.get("#checkboxes > :nth-child(1) > input").as("Option-1");
-    cy.get("@Option-1").check().should("be.checked");
+    cy.get("#checkboxes > :nth-child(1) > input").as("option-1");
+    //cy.get('@option-1').check();
+    cy.get("@option-1").check().should("be.checked");
   });
 
-  it.only("Uncheck and validate checkboxes", () => {
-    // Open webdriveruniversity homepage
-    cy.visit("http://www.webdriveruniversity.com");
-    // In order for openning the tab inside the current window we need to remove the target attribute and value JQuery Method of removeAttrb
-    cy.get("#dropdown-checkboxes-radiobuttons")
-      .invoke("removeAttr", "target")
-      .click({ force: true });
-    cy.get(":nth-child(5) > input").as("Option-3");
-    cy.get("@Option-3").should("be.checked");
-    // Check the Option-3
-    cy.get("@Option-3").uncheck().should("not.be.checked");
+  it("Uncheck and validate checkbox", () => {
+    cy.get(":nth-child(5) > input").as("option-3");
+    cy.get("@option-3").uncheck().should("not.be.checked");
   });
 
-  it.only("Check multiple checkboxes", () => {
-    // Open webdriveruniversity homepage
-    cy.visit("http://www.webdriveruniversity.com");
-    // In order for openning the tab inside the current window we need to remove the target attribute and value JQuery Method of removeAttrb
-    cy.get("#dropdown-checkboxes-radiobuttons")
-      .invoke("removeAttr", "target")
-      .click({ force: true });
-    cy.get("input[type='checkbox']").as("checkbox");
-    cy.get("@checkbox")
+  it("Check mutiple checkboxes", () => {
+    cy.get("input[type='checkbox']")
       .check(["option-1", "option-2", "option-3", "option-4"])
       .should("be.checked");
   });
--- cypress/integration/webdriver-university/radiobutton.js
+++ cypress/integration/webdriver-university/radiobutton.js
@@ -1,38 +1,26 @@
 /// <reference types="cypress" />
 
-describe("Handling radiobuttons", () => {
-  it("Verify radiobuttons in Webdriveruni", () => {
-    // Open webdriveruniversity homepage
+describe("Verify radio buttons via webdriveruni", () => {
+  before(() => {
     cy.visit("http://www.webdriveruniversity.com");
-    // In order for openning the tab inside the current window we need to remove the target attribute and value JQuery Method of removeAttrb
     cy.get("#dropdown-checkboxes-radiobuttons")
       .invoke("removeAttr", "target")
       .click({ force: true });
-
-    cy.get("[id='radio-buttons']").find("[type='radio']").first().check();
-    // Check radio button blue (Index number 1)
-    cy.get("[id='radio-buttons']").find("[type='radio']").eq(1).check();
-
-    // Check radio button purple (Index number 4)
-    cy.get("[id='radio-buttons']").find("[type='radio']").eq(4).check();
   });
 
-  it("Verify state of specific radio buttons", () => {
-    // Open webdriveruniversity homepage
-    cy.visit("http://www.webdriveruniversity.com");
-    // In order for openning the tab inside the current window we need to remove the target attribute and value JQuery Method of removeAttrb
-    cy.get("#dropdown-checkboxes-radiobuttons")
-      .invoke("removeAttr", "target")
-      .click({ force: true });
-    // Verify that the first radio button is not checked
+  it("Check specific radio buttons", () => {
+    cy.get("#radio-buttons").find("[type='radio']").first().check();
+    cy.get("#radio-buttons").find("[type='radio']").eq(1).check();
+  });
+
+  it("Validate the states of specific radio buttons", () => {
     cy.get("[value='lettuce']").should("not.be.checked");
-    // Check the first radio button
+    cy.get("[value='pumpkin']").should("be.checked");
+
     cy.get("[value='lettuce']").check();
-    // Verify that the second radio button is disabled
-    cy.get("[value='cabbage']").should("be.disabled");
-    // Verify that the third radio button is checked
+    cy.get("[value='lettuce']").should("be.checked");
     cy.get("[value='pumpkin']").should("not.be.checked");
-    // Uncheck the third radio button
-    cy.get("[value='pumpkin']").check();
+
+    cy.get("[value='cabbage']").should("be.disabled");
   });
 });
